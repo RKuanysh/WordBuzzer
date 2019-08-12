9cf58a4d3af0f3a3907b729d5cb304bc897a0adf
Kuanysh <gitlab.com@k.otherinbox.com>
2019-08-12 12:25:00 +0600

    added wrong translations with random words from repository

--- app/src/main/java/com/raimbekov/wordbuzzer/word/domain/WordInteractor.kt
+++ app/src/main/java/com/raimbekov/wordbuzzer/word/domain/WordInteractor.kt
@@ -3,6 +3,7 @@ package com.raimbekov.wordbuzzer.word.domain
 import com.raimbekov.wordbuzzer.game.model.Question
 import io.reactivex.Completable
 import io.reactivex.Single
+import kotlin.random.Random
 
 class WordInteractor(
     private val wordRepository: WordRepository
@@ -13,9 +14,16 @@ class WordInteractor(
     fun getQuestions(numberOfQuestions: Int): Single<List<Question>> =
         Single.fromCallable {
             val result = mutableListOf<Question>()
+            val size = wordRepository.getSize()
+
             for (i in 0..numberOfQuestions) {
-                result.add(Question(wordRepository.getWord(i), wordRepository.getWord(i)))
+                val index = Random.nextInt(size)
+                val isCorrect = Random.nextBoolean()
+
+                val translationIndex = if (isCorrect) index else Random.nextInt(size)
+                result.add(Question(wordRepository.getWord(index), wordRepository.getWord(translationIndex)))
             }
+
             result
         }
 }
